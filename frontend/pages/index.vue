ab6feb3825b53baf03b3f222c2df69fc3fb7a4f7
Sergei Ozerov <s.ozerov@smartex-it.com>
2019-04-10 11:03:16 +0300

    front

---
+++ frontend/pages/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-layout
+    column
+    justify-center
+    align-center
+  >
+    <v-flex
+      xs12
+      sm8
+      md6
+    >
+      <v-card class="main-text">
+        <!--TODO: Edit it properly and maintainably-->
+        <h1>PIІMO</h1>
+
+        <h2>Aplikacja, z ktуrє szybko stworzysz kaњdy oficjalny dokument.</h2>
+        <br>
+        <p><b>PIІMO to edytor do tworzenia:</b></p>
+        <ul>
+          <li>wnioskуw o dostкp do informacji publicznej</li>
+          <li>podaс</li>
+          <li>zaњaleс</li>
+          <li>odwo≥aс</li>
+          <li>skarg</li>
+          <li>wnioskуw</li>
+          <li>ponagleс</li>
+        </ul>
+        <br>
+        <p>PIІMO oferuje nieodp≥atny zbiуr szablonуw wraz z odpowiedniє podstawє prawnє na kaњdє okazjк. Wystarczy wype≥niж niezbкdne pola,
+          a aplikacja sama dopasuje Twoje dane do wybranego szablonu, tworzєc gotowy edytowalny dokument.</p>
+
+        <p>Zapisuj, edytuj, drukuj i twуrz pliki PDF. Nie potrzebujesz specjalistycznej prawniczej wiedzy,
+          aby dochodziж swoich praw przed administracjє publicznє.</p>
+      </v-card>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+
+export default {
+  components: {
+  }
+}
+</script>
+
+<style scoped>
+  .main-text {
+    padding: 15px
+  }
+</style>
---
+++ frontend/store/index.js
@@ -0,0 +1,22 @@
+export const state = () => ({
+  locales: ['pl', 'en'],
+  locale: 'pl'
+})
+
+export const mutations = {
+  SET_LANG(state, locale) {
+    if (state.locales.indexOf(locale) !== -1) {
+      state.locale = locale
+    }
+  }
+}
+
+export const actions = {
+  nuxtServerInit({ dispatch, state }, { params }) {
+    return Promise.all([
+      dispatch('slides/load', params),
+      dispatch('menu/load', params),
+      dispatch('site/load', params)
+    ])
+  }
+}
