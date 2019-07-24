946a7a2641ea6441f85cb24f7372f5e3fb91ff99
Marcin Porycki <marcin.porycki@growbots.com>
2019-07-25 00:51:08 +0200

    Add more unit tests

--- Tests/Unit/test_documenter.py
+++ Tests/Unit/test_documenter.py
@@ -11,7 +11,7 @@ def test_doc():
     obj = PDFDocument(
         "41c3b833-cb4e-4e31-a499-422a3ca6a2be",
         "07054204-2a9a-4146-9942-47b077b557a7",
-        {"custom_field1": "abc"},
+        {"custom_field1": "abc", "custom_field2": "see"},
     )
     return obj
 
@@ -24,22 +24,81 @@ def tempfile(filename: str, create: bool):
     os.remove(filename)
 
 
-def test_create_filename_creates_correct_name(monkeypatch):
-    pass
+def test_create_filename_creates_correct_name(test_doc, monkeypatch):
+    # GIVEN
+    monkeypatch.setattr(
+        "Modules.Documenter.PDFDocument.PDFDocument."
+        "get_username_and_template",
+        lambda x: ("test", "Test_template"),
+    )
+
+    # WHEN
+    test_filename = test_doc.create_filename()
+
+    # THEN
+    assert test_filename == "test_Test_template.pdf"
 
 
-def test_fill_form_creates_correct_text():
-    pass
+def test_fill_form_creates_correct_text_when_correct_kwargs_are_given(
+    test_doc, monkeypatch
+):
+    # GIVEN
+    monkeypatch.setattr(
+        "Modules.Documenter.PDFDocument.PDFDocument.get_form_text",
+        lambda x: "Hello {custom_field1}, long time no {custom_field2}",
+    )
+
+    # WHEN
+    test_form = test_doc.fill_form()
+
+    # THEN
+    assert test_form == "Hello abc, long time no see"
+
+
+def test_fill_form_creates_correct_text_when_too_many_kwargs_are_given(
+    test_doc, monkeypatch
+):
+    # GIVEN
+    monkeypatch.setattr(
+        "Modules.Documenter.PDFDocument.PDFDocument.get_form_text",
+        lambda x: "Hello {custom_field1}, long time no {custom_field2}",
+    )
+    test_doc.custom_fields["custom_field3"] = "fake"
+
+    # WHEN
+    test_form = test_doc.fill_form()
+
+    # THEN
+    assert test_form == "Hello abc, long time no see"
+
+
+def test_fill_form_raises_400_when_not_enough_kwargs_are_given(
+    test_doc, monkeypatch
+):
+    # GIVEN
+    monkeypatch.setattr(
+        "Modules.Documenter.PDFDocument.PDFDocument.get_form_text",
+        lambda x: "Hello {custom_field1}, long time no {custom_field2}",
+    )
+    test_doc.custom_fields.pop("custom_field2")
+
+    # WHEN
+    test_form = test_doc.fill_form()
+
+    # THEN
+    assert test_form == "Hello abc, long time no see"
 
 
 def test_create_file_creates_file(test_doc, monkeypatch):
     # GIVEN
     monkeypatch.setattr(
-        "Modules.Documenter.PDFDocument.PDFDocument.fill_form", lambda x: "Test"
+        "Modules.Documenter.PDFDocument.PDFDocument.fill_form",
+        lambda x: "Test",
     )
     monkeypatch.setattr(
         "Modules.Documenter.PDFDocument.PDFDocument.create_filename",
-        lambda x: "Test_abc.pdf")
+        lambda x: "Test_abc.pdf",
+    )
     test_filename = "Test_abc.pdf"
 
     # WHEN
